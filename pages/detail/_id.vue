ea4e91b68efb40961a9afc8db34480c8c3633bbb
데이터지능화팀/장우재 <woojae.jang@hyundai-autoever.com>
2022-11-03 13:29:24 +0900

    description 변경 확인

--- pages/detail/_id.vue
+++ pages/detail/_id.vue
@@ -34,7 +34,7 @@ export default {
     title: 'Shopping Item Detail',
     meta: [
       {
-        hid: 'description',
+        hid: 'description12',
         name: 'description',
         content: '이 상품은 ~~입니다.',
       },
